9a6f01cbcb0c42e83884ce16cae5d8bb4d328688
kurban-4 <annagurban33@gmail.com>
2026-05-20 01:08:32 +0500

    fixed the expense method and added some changingPrice functions

--- src-tauri/resources/rysgally-hasap-market/config/app.php
+++ src-tauri/resources/rysgally-hasap-market/config/app.php
@@ -8,7 +8,7 @@ return [
 
     'debug' => (bool) env('APP_DEBUG', false),
 
-    'url' => env('APP_URL', 'http:
+    'url' => env('APP_URL', 'http://localhost'),
 
     'timezone' => 'Asia/Ashgabat',
 
