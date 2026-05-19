2a23a2b50ca8c41db42d96346008a87aa42c3ed2
kurban-4 <annagurban33@gmail.com>
2026-05-20 02:09:48 +0500

    Fixing the bugs

--- vite.config.js
+++ vite.config.js
@@ -12,23 +12,19 @@ export default defineConfig(async () => ({
   // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
-// vite.config.js
-server: {
-  port: 5173, // ИЗМЕНИ ЗДЕСЬ С 8001 НА 5173
-  strictPort: true,
-  host: true,
-  hmr: host
-    ? {
-        protocol: "ws",
-        host,
-        port: 1421,
-      }
-    : undefined,
-  watch: {
-    ignored: ["**/src-tauri/**"],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**", "**/resources/**"],
+    },
   },
-  watch: {
-    ignored: ["**/src-tauri/**", "**/resources/**"],
-},
-},
-}));
+}))
